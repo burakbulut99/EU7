ae3e31fe885bd7bd5093016922ae96dd4998461a
burakbulut99 <burakbulut99@gmail.com>
2022-01-02 17:59:17 +0100

    Dropdown completed

--- src/test/java/com/cybertek/test/DropdownTest.java
+++ src/test/java/com/cybertek/test/DropdownTest.java
@@ -20,7 +20,7 @@ public class DropdownTest {
      Task3:
     1. Go to:  http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
     2. Login with username: Tester, password: test
-    3. Click  Order button
+    3. Click  Order butto
     4. Verify under Product Information, selected option is “MyMoney”
     5. Then select FamilyAlbum, make quantity 2, and click Calculate,
     6. Then verify Total is equal to Quantity*PricePerUnit
